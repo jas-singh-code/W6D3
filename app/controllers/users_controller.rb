89775cb8f9ad32fbdda29fc43818f68271696ffe
jas-singh-code <jaspreetsingh7798@gmail.com>
2021-02-03 16:43:44 -0500

    finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,33 @@
 class UsersController < ApplicationController
     def index
-        render plain: "Im in the index action"
+        # render plain: "Im in the index action"
+        render json: user
     end
 
     def create
-        render json: params
+        user = User.new(params.require(:user).permit(:name, :email))
+        if user.save!
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unporcessable_entity
+        end
     end
 
     def show
-        render json: params
+        user= User.find(params[:id])
+        render json: user
+    end
+
+    def update
+        debugger
+        user= User.find(params[:id])
+        user.update
+        render json: user
+    end
+
+    def destroy
+        user= User.find(params[:id])
+        user.destroy
+        render json: user
     end
 end
